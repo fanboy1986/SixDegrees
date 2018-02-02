22e0dfb35c2ad2338711db001bdfbebe2aba1885
Benjamin Haynes <behaynes@microsoft.com>
2018-02-01 17:16:25 -0800

    Add project files.

---
+++ SixDegrees/SixDegrees/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using SixDegrees.Models;
+
+namespace SixDegrees.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
